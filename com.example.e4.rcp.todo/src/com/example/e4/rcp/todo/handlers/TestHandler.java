5444a842085dba2ca3a8d16ffb7d58d4ca16782c
Lars Vogel <Lars.Vogel@gmail.com>
2012-07-05 09:45:20 +0200

    implemeted the testhandler to change the IWindowCloseHandler

--- com.example.e4.rcp.todo/src/com/example/e4/rcp/todo/handlers/TestHandler.java
+++ com.example.e4.rcp.todo/src/com/example/e4/rcp/todo/handlers/TestHandler.java
@@ -1,16 +1,23 @@
 package com.example.e4.rcp.todo.handlers;
 
-import org.eclipse.e4.core.di.annotations.CanExecute;
 import org.eclipse.e4.core.di.annotations.Execute;
+import org.eclipse.e4.ui.model.application.ui.basic.MWindow;
+import org.eclipse.e4.ui.workbench.modeling.EModelService;
+import org.eclipse.e4.ui.workbench.modeling.IWindowCloseHandler;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.swt.widgets.Shell;
 
 public class TestHandler {
 	@Execute
-	public void execute() {
-		System.out.println("Called");
+	public void execute(final Shell shell, EModelService service, MWindow window) {
+		IWindowCloseHandler handler = new IWindowCloseHandler() {
+			@Override
+			public boolean close(MWindow window) {
+				return MessageDialog.openConfirm(shell, "Close",
+						"You will loose data. Really close?");
+			}
+		};
+		window.getContext().set(IWindowCloseHandler.class, handler);
 	}
 
-	@CanExecute
-	public boolean canExecute() {
-		return true;
-	}
 }
