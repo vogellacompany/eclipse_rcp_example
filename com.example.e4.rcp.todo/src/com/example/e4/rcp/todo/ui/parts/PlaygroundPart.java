0f1c39cf635e59bbd96cc708afaecb965fd51222
Lars Vogel <Lars.Vogel@gmail.com>
2012-07-05 06:11:36 +0200

    Part lifecycle implemented

--- com.example.e4.rcp.todo/src/com/example/e4/rcp/todo/ui/parts/PlaygroundPart.java
+++ com.example.e4.rcp.todo/src/com/example/e4/rcp/todo/ui/parts/PlaygroundPart.java
@@ -1,5 +1,25 @@
 package com.example.e4.rcp.todo.ui.parts;
 
+import javax.annotation.PostConstruct;
+
+import org.eclipse.e4.ui.di.Focus;
+import org.eclipse.e4.ui.di.Persist;
+import org.eclipse.e4.ui.model.application.ui.MDirtyable;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.ModifyEvent;
+import org.eclipse.swt.events.ModifyListener;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Text;
+
 public class PlaygroundPart {
 
+	// Declare a field label, required for @Focus
+		Label label;
+
+		@PostConstruct
+		public void createControls(Composite parent) {
+			label = new Label(parent, SWT.NONE);
+		}
 }
--- com.example.e4.rcp.todo/src/com/example/e4/rcp/todo/ui/parts/TodoOverviewPart.java
+++ com.example.e4.rcp.todo/src/com/example/e4/rcp/todo/ui/parts/TodoOverviewPart.java
@@ -1,5 +1,17 @@
 package com.example.e4.rcp.todo.ui.parts;
 
+import javax.annotation.PostConstruct;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
+
 public class TodoOverviewPart {
+	// Declare a field label, required for @Focus
+	Label label;
+	@PostConstruct
+	public void createControls(Composite parent) {
+	label = new Label(parent,SWT.NONE);
+	}
 
 }
