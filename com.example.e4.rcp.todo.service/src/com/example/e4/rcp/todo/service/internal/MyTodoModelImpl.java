d9acbac995c8eb1b0989da50a9a0499f7222d0fc
Lars Vogel <Lars.Vogel@gmail.com>
2012-07-05 10:47:36 +0200

    Creates OSGi ds services for data model

---
+++ com.example.e4.rcp.todo.service/src/com/example/e4/rcp/todo/service/internal/MyTodoModelImpl.java
@@ -0,0 +1,63 @@
+package com.example.e4.rcp.todo.service.internal;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import com.example.e4.rcp.todo.model.ITodoModel;
+import com.example.e4.rcp.todo.model.Todo;
+
+public class MyTodoModelImpl implements ITodoModel {
+
+	static int current = 1;
+	private List<Todo> model;
+
+	public MyTodoModelImpl() {
+		model = createInitialModel();
+	}
+
+	// Always return a new copy of the data
+	@Override
+	public List<Todo> getTodos() {
+		ArrayList<Todo> list = new ArrayList<Todo>();
+		for (Todo todo : model) {
+			list.add(todo.copy());
+		}
+		return list;
+	}
+
+	@Override
+	public boolean saveTodo(Todo todo) {
+		throw new UnsupportedOperationException("Not supported yet");
+	}
+
+	@Override
+	public Todo getTodo(long id) {
+		for (Todo todo : model) {
+			if (todo.getId() == id) {
+				return todo.copy();
+			}
+		}
+		return null;
+	}
+
+	@Override
+	public boolean deleteTodo(long id) {
+		throw new UnsupportedOperationException("Not supported yet");
+	}
+
+	// Example data, change if you like
+	private List<Todo> createInitialModel() {
+		ArrayList<Todo> list = new ArrayList<Todo>();
+		list.add(createTodo("SWT", "Learn Widgets"));
+		list.add(createTodo("JFace", "Especially Viewers!"));
+		list.add(createTodo("DI", "@Inject looks interesting"));
+		list.add(createTodo("OSGi", "Services"));
+		list.add(createTodo("Compatibility Layer", "Run Eclipse 3.x"));
+		return list;
+	}
+
+	private Todo createTodo(String summary, String description) {
+		return new Todo(current++, summary, description, false, new Date());
+	}
+}
--- com.example.e4.rcp.todo/src/com/example/e4/rcp/todo/ui/parts/TodoOverviewPart.java
+++ com.example.e4.rcp.todo/src/com/example/e4/rcp/todo/ui/parts/TodoOverviewPart.java
@@ -6,12 +6,16 @@ import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Label;
 
+import com.example.e4.rcp.todo.model.ITodoModel;
+
 public class TodoOverviewPart {
 	// Declare a field label, required for @Focus
 	Label label;
+
 	@PostConstruct
-	public void createControls(Composite parent) {
-	label = new Label(parent,SWT.NONE);
+	public void createControls(Composite parent, ITodoModel model) {
+		System.out.println(model.getTodos().size());
+		label = new Label(parent, SWT.NONE);
 	}
 
 }
