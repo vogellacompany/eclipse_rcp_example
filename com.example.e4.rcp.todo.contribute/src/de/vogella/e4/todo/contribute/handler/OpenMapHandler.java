eb3852a93afcad1ce4183b19b895ce6fb6fad14e
Lars Vogel <Lars.Vogel@gmail.com>
2012-07-09 11:48:59 +0200

    Added the fragment example

---
+++ com.example.e4.rcp.todo.contribute/src/de/vogella/e4/todo/contribute/handler/OpenMapHandler.java
@@ -0,0 +1,12 @@
+package de.vogella.e4.todo.contribute.handler;
+
+import org.eclipse.e4.core.di.annotations.Execute;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.swt.widgets.Shell;
+
+public class OpenMapHandler {
+	@Execute
+	public void run(Shell shell) {
+		MessageDialog.openInformation(shell, "Just testing", "Test");
+	}
+}
