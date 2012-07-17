dbb7fb3a1b8ba3f3a8cb6d39a928d75d9c258feb
Lars Vogel <Lars.Vogel@gmail.com>
2012-07-17 11:25:01 +0200

    Build the initial SWT user interface for TodoOverviewPart

--- com.example.e4.rcp.todo/src/com/example/e4/rcp/todo/ui/parts/TodoOverviewPart.java
+++ com.example.e4.rcp.todo/src/com/example/e4/rcp/todo/ui/parts/TodoOverviewPart.java
@@ -2,20 +2,46 @@ package com.example.e4.rcp.todo.ui.parts;
 
 import javax.annotation.PostConstruct;
 
+import org.eclipse.e4.ui.di.Focus;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Label;
 
 import com.example.e4.rcp.todo.model.ITodoModel;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
 
 public class TodoOverviewPart {
-	// Declare a field label, required for @Focus
-	Label label;
+
+	private Button btnNewButton;
+	private Label lblNewLabel;
 
 	@PostConstruct
 	public void createControls(Composite parent, ITodoModel model) {
 		System.out.println(model.getTodos().size());
-		label = new Label(parent, SWT.NONE);
+		parent.setLayout(new GridLayout(2, false));
+		
+		btnNewButton = new Button(parent, SWT.NONE);
+		btnNewButton.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				lblNewLabel.setText("Number of Todo items: 7");
+			}
+		});
+		
+		btnNewButton.setText("Load Data");
+		
+		lblNewLabel = new Label(parent, SWT.NONE);
+		lblNewLabel.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+		lblNewLabel.setText("Number of Todo Items: 0");
 	}
-
+	
+	@Focus
+	private void setFocus() {
+		btnNewButton.setFocus();
+	}
+	
 }
