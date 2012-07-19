4020b7834a4bee768201899886c3e7e95d7970c2
Lars Vogel <Lars.Vogel@gmail.com>
2012-07-19 16:40:16 +0200

    SWT User interface for TodoDetailsPart

--- com.example.e4.rcp.todo/src/com/example/e4/rcp/todo/ui/parts/TodoDetailsPart.java
+++ com.example.e4.rcp.todo/src/com/example/e4/rcp/todo/ui/parts/TodoDetailsPart.java
@@ -5,13 +5,47 @@ import javax.annotation.PostConstruct;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.widgets.DateTime;
+import org.eclipse.swt.widgets.Button;
 
 public class TodoDetailsPart {
-	// Declare a field label, required for @Focus
-	Label label;
+	private Text text;
+	private Text text_1;
 
 	@PostConstruct
 	public void createControls(Composite parent) {
-		label = new Label(parent, SWT.NONE);
+		GridLayout gl_parent = new GridLayout(2, false);
+		gl_parent.marginRight = 10;
+		gl_parent.marginLeft = 10;
+		gl_parent.horizontalSpacing = 10;
+		gl_parent.marginWidth = 0;
+		parent.setLayout(gl_parent);
+		
+		Label lblSummary = new Label(parent, SWT.NONE);
+		lblSummary.setText("Summary");
+		
+		text = new Text(parent, SWT.BORDER);
+		text.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+		
+		Label lblDescription = new Label(parent, SWT.NONE);
+		lblDescription.setText("Description");
+		
+		text_1 = new Text(parent, SWT.BORDER);
+		GridData gd_text_1 = new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1);
+		gd_text_1.heightHint = 122;
+		text_1.setLayoutData(gd_text_1);
+		
+		Label lblNewLabel = new Label(parent, SWT.NONE);
+		lblNewLabel.setText("Due Date");
+		
+		DateTime dateTime = new DateTime(parent, SWT.BORDER);
+		dateTime.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false, false, 1, 1));
+		new Label(parent, SWT.NONE);
+		
+		Button btnDone = new Button(parent, SWT.CHECK);
+		btnDone.setText("Done");
 	}
 }
