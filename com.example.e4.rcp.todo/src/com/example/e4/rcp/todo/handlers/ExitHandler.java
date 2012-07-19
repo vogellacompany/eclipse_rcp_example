0ec86865307c7c69db47c27b0f90223350ad2018
Lars Vogel <Lars.Vogel@gmail.com>
2012-07-19 16:40:52 +0200

    Open a confirmation Dialog in the Exit Handler

--- com.example.e4.rcp.todo/src/com/example/e4/rcp/todo/handlers/ExitHandler.java
+++ com.example.e4.rcp.todo/src/com/example/e4/rcp/todo/handlers/ExitHandler.java
@@ -1,13 +1,20 @@
 package com.example.e4.rcp.todo.handlers;
 
-import org.eclipse.e4.core.di.annotations.CanExecute;
 import org.eclipse.e4.core.di.annotations.Execute;
 import org.eclipse.e4.ui.workbench.IWorkbench;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.window.Window;
+import org.eclipse.swt.widgets.Shell;
 
 public class ExitHandler {
 	@Execute
-	public void execute(IWorkbench workbench) {
-		workbench.close();
-	}
+	public void execute(IWorkbench workbench, Shell shell) {
+		boolean result =
+			MessageDialog.openConfirm(shell, "Close",
+				"Close application?");
+		if (result) {
+			workbench.close();
+		}
 
+	}
 }
