ac2636c3d9400def1b6b2c85bfcb9c6677d6f394
Lars Vogel <Lars.Vogel@gmail.com>
2012-07-20 00:05:20 +0200

    Implemented databinding for the TodoOverviewPart

--- com.example.e4.rcp.todo/src/com/example/e4/rcp/todo/ui/parts/PlaygroundPart.java
+++ com.example.e4.rcp.todo/src/com/example/e4/rcp/todo/ui/parts/PlaygroundPart.java
@@ -2,51 +2,54 @@ package com.example.e4.rcp.todo.ui.parts;
 
 import javax.annotation.PostConstruct;
 
+import org.eclipse.core.databinding.DataBindingContext;
+import org.eclipse.core.databinding.observable.value.IObservableValue;
 import org.eclipse.e4.ui.di.Focus;
-import org.eclipse.e4.ui.di.Persist;
 import org.eclipse.e4.ui.model.application.ui.MDirtyable;
+import org.eclipse.jface.databinding.swt.ISWTObservableValue;
+import org.eclipse.jface.databinding.swt.WidgetProperties;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.events.ModifyEvent;
-import org.eclipse.swt.events.ModifyListener;
 import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Text;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.core.databinding.beans.PojoProperties;
 
 public class PlaygroundPart {
-	private Text text;
+	private DataBindingContext m_bindingContext;
+	private Text eins;
+	private Text zwei;
 
 	@PostConstruct
 	public void createControls(Composite parent, final MDirtyable dirty) {
+		parent.setLayout(new GridLayout(1, false));
+		eins = new Text(parent, SWT.BORDER);
+		eins.setText("Hello1");
+		eins.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+		zwei = new Text(parent, SWT.BORDER);
+		zwei.setText("Hello2");
+		zwei.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
 		
-		text = new Text(parent, SWT.MULTI | SWT.LEAD | SWT.BORDER);
-		text.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false));
-		text.setSize(200, 100);
-		text.addModifyListener(new ModifyListener() {
-			
-			@Override
-			public void modifyText(ModifyEvent e) {
-				dirty.setDirty(true);
-			}
-		});
-		
+		Button btnClick = new Button(parent, SWT.CHECK);
+		btnClick.setText("Click");
+		m_bindingContext = initDataBindings();
+
 	}
+	
 	@Focus
 	public void setFocus() {
-		text.setFocus();
+		eins.setFocus();
 	}
 	
-	@Persist
-	public void persists(MDirtyable dirty) {
-		dirty.setDirty(false);
-		System.out.println("PlaygroundPart data persisted...");
+	protected DataBindingContext initDataBindings() {
+		DataBindingContext bindingContext = new DataBindingContext();
+		//
+		IObservableValue observeTextEinsObserveWidget = WidgetProperties.text(SWT.Modify).observe(eins);
+		IObservableValue textZweiObserveValue = PojoProperties.value("text").observe(zwei);
+		bindingContext.bindValue(observeTextEinsObserveWidget, textZweiObserveValue, null, null);
+		//
+		return bindingContext;
 	}
-	
-	// Declare a field label, required for @Focus
-		Label label;
-
-		@PostConstruct
-		public void createControls(Composite parent) {
-			label = new Label(parent, SWT.NONE);
-		}
 }
--- com.example.e4.rcp.todo/src/com/example/e4/rcp/todo/ui/parts/TodoOverviewPart.java
+++ com.example.e4.rcp.todo/src/com/example/e4/rcp/todo/ui/parts/TodoOverviewPart.java
@@ -1,44 +1,42 @@
 package com.example.e4.rcp.todo.ui.parts;
 
-import java.net.URL;
+import java.util.List;
 
 import javax.annotation.PostConstruct;
 
-import org.eclipse.core.runtime.FileLocator;
-import org.eclipse.core.runtime.Path;
-import org.eclipse.core.runtime.internal.adaptor.IModel;
+import org.eclipse.core.databinding.DataBindingContext;
+import org.eclipse.core.databinding.beans.PojoProperties;
+import org.eclipse.core.databinding.observable.list.IObservableList;
+import org.eclipse.core.databinding.property.Properties;
 import org.eclipse.e4.ui.di.Focus;
-import org.eclipse.jface.resource.ImageDescriptor;
-import org.eclipse.jface.viewers.ArrayContentProvider;
-import org.eclipse.jface.viewers.CellEditor;
-import org.eclipse.jface.viewers.ColumnLabelProvider;
-import org.eclipse.jface.viewers.EditingSupport;
+import org.eclipse.jface.databinding.viewers.ViewerSupport;
 import org.eclipse.jface.viewers.TableViewer;
-import org.eclipse.jface.viewers.TableViewerColumn;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
-import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Table;
 import org.eclipse.swt.widgets.TableColumn;
-import org.osgi.framework.Bundle;
-import org.osgi.framework.FrameworkUtil;
 
 import com.example.e4.rcp.todo.model.ITodoModel;
 import com.example.e4.rcp.todo.model.Todo;
-import org.eclipse.swt.layout.GridData;
 
 public class TodoOverviewPart {
+	private DataBindingContext m_bindingContext;
 
 	private Button btnNewButton;
 	private Label lblNewLabel;
+	private TableViewer viewer;
+
+	private List<Todo> list;
 
 	@PostConstruct
 	public void createControls(Composite parent, ITodoModel model) {
+		 list = model.getTodos();
 		System.out.println(model.getTodos().size());
 		parent.setLayout(new GridLayout(1, false));
 
@@ -52,43 +50,21 @@ public class TodoOverviewPart {
 
 		btnNewButton.setText("Load Data");
 
-		TableViewer viewer = new TableViewer(parent, SWT.MULTI | SWT.H_SCROLL
+		viewer = new TableViewer(parent, SWT.MULTI | SWT.H_SCROLL
 				| SWT.V_SCROLL | SWT.FULL_SELECTION);
 		Table table = viewer.getTable();
 		table.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
 		table.setHeaderVisible(true);
 		table.setLinesVisible(true);
-
-		viewer.setContentProvider(ArrayContentProvider.getInstance());
-
-		TableViewerColumn column = new TableViewerColumn(viewer, SWT.NONE);
-		TableColumn tableColumn = column.getColumn();
-		tableColumn.setWidth(200);
-		tableColumn.setText("Summary");
-		column.setLabelProvider(new ColumnLabelProvider() {
-			public String getText(Object element) {
-				Todo todo = (Todo) element;
-
-				return todo.getSummary();
-			};
-		});
-		
-		column = new TableViewerColumn(viewer, SWT.NONE);
-		tableColumn = column.getColumn();
-		tableColumn.setWidth(200);
-		tableColumn.setText("Description");
-		column.setLabelProvider(new ColumnLabelProvider() {
-			public String getText(Object element) {
-				Todo todo = (Todo) element;
-
-				return todo.getDescription();
-			};
-		}
-
-		);
-		
-		viewer.setInput(model.getTodos());
-
+		TableColumn column = new TableColumn(table, SWT.NONE);
+		column.setWidth(200);
+		column = new TableColumn(table, SWT.NONE);
+		column.setWidth(200);
+		column = new TableColumn(table, SWT.NONE);
+		column.setWidth(200);
+		column = new TableColumn(table, SWT.NONE);
+		column.setWidth(200);
+		m_bindingContext = initDataBindings();
 	}
 
 	@Focus
@@ -98,4 +74,14 @@ public class TodoOverviewPart {
 
 	
 
+
+	
+	protected DataBindingContext initDataBindings() {
+		DataBindingContext bindingContext = new DataBindingContext();
+		//
+		IObservableList selfList = Properties.selfList(Todo.class).observe(list);
+		ViewerSupport.bind(viewer, selfList, PojoProperties.values(Todo.class, new String[]{"summary", "description", "done", "dueDate"}));
+		//
+		return bindingContext;
+	}
 }
