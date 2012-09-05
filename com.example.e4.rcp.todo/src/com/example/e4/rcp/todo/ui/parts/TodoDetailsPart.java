c549330964c321e82e1e5032c44c4059c0a83391
Lars Vogel <Lars.Vogel@gmail.com>
2012-09-05 23:52:52 +0200

    Layout fix for details part and check if dirty is NULL or not

--- com.example.e4.rcp.todo/src/com/example/e4/rcp/todo/ui/parts/TodoDetailsPart.java
+++ com.example.e4.rcp.todo/src/com/example/e4/rcp/todo/ui/parts/TodoDetailsPart.java
@@ -1,19 +1,32 @@
 package com.example.e4.rcp.todo.ui.parts;
 
 import javax.annotation.PostConstruct;
+import javax.inject.Inject;
+import javax.inject.Named;
 
+import org.eclipse.e4.core.di.annotations.Optional;
+import org.eclipse.e4.ui.di.Persist;
+import org.eclipse.e4.ui.model.application.ui.MDirtyable;
+import org.eclipse.e4.ui.services.IServiceConstants;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Label;
-import org.eclipse.swt.layout.GridLayout;
-import org.eclipse.swt.widgets.Text;
+import org.eclipse.swt.events.ModifyEvent;
+import org.eclipse.swt.events.ModifyListener;
 import org.eclipse.swt.layout.GridData;
-import org.eclipse.swt.widgets.DateTime;
+import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.DateTime;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Text;
+
+import com.example.e4.rcp.todo.model.Todo;
 
 public class TodoDetailsPart {
-	private Text text;
-	private Text text_1;
+	private Text summary;
+	private Text description;
+	@Inject
+	MDirtyable dirty;
+	private Todo todo;
 
 	@PostConstruct
 	public void createControls(Composite parent) {
@@ -23,29 +36,68 @@ public class TodoDetailsPart {
 		gl_parent.horizontalSpacing = 10;
 		gl_parent.marginWidth = 0;
 		parent.setLayout(gl_parent);
-		
+
 		Label lblSummary = new Label(parent, SWT.NONE);
 		lblSummary.setText("Summary");
-		
-		text = new Text(parent, SWT.BORDER);
-		text.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
-		
+
+		summary = new Text(parent, SWT.BORDER);
+		summary.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+		summary.addModifyListener(new ModifyListener() {
+			@Override
+			public void modifyText(ModifyEvent e) {
+				if (dirty!=null) {
+					dirty.setDirty(true);
+				}
+				// Sei bereit zum speichern
+			}
+		});
+
 		Label lblDescription = new Label(parent, SWT.NONE);
 		lblDescription.setText("Description");
-		
-		text_1 = new Text(parent, SWT.BORDER);
-		GridData gd_text_1 = new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1);
+
+		description = new Text(parent, SWT.BORDER| SWT.MULTI);
+		GridData gd_text_1 = new GridData(SWT.FILL, SWT.CENTER, true, false, 1,
+				1);
 		gd_text_1.heightHint = 122;
-		text_1.setLayoutData(gd_text_1);
-		
+		description.setLayoutData(gd_text_1);
+
 		Label lblNewLabel = new Label(parent, SWT.NONE);
 		lblNewLabel.setText("Due Date");
-		
+
 		DateTime dateTime = new DateTime(parent, SWT.BORDER);
-		dateTime.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false, false, 1, 1));
+		dateTime.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false, false,
+				1, 1));
 		new Label(parent, SWT.NONE);
-		
+
 		Button btnDone = new Button(parent, SWT.CHECK);
 		btnDone.setText("Done");
 	}
+
+	@Persist
+	public void save(MDirtyable dirty) {
+		// TODO speichern...
+		dirty.setDirty(false);
+	}
+
+	@Inject
+	public void setTodo(
+			@Optional @Named(IServiceConstants.ACTIVE_SELECTION) Todo todo) {
+		if (todo != null) {
+			// Remember the todo as field
+			this.todo = todo;
+			// update the user interface
+			updateUserInterface(todo);
+		}
+	}
+
+	private void updateUserInterface(Todo todo) {
+		// Check if Ui is available
+		// Assumes that one of your fields
+		// is called summary
+		if (summary != null && !summary.isDisposed()) {
+			summary.setText(todo.getSummary());
+			// TODO more updates
+		}
+	}
+
 }
