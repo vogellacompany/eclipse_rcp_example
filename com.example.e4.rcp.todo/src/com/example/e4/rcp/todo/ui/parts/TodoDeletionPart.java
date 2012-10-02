a2530dc9f0223b313338c8748c57c56f2a867f79
Lars Vogel <Lars.Vogel@gmail.com>
2012-10-02 21:10:38 +0200

    Added TodoDeletionPart to the Application Model and implemented the deletion logic.

--- com.example.e4.rcp.todo/src/com/example/e4/rcp/todo/ui/parts/TodoDeletionPart.java
+++ com.example.e4.rcp.todo/src/com/example/e4/rcp/todo/ui/parts/TodoDeletionPart.java
@@ -1,38 +1,79 @@
 package com.example.e4.rcp.todo.ui.parts;
 
-import java.text.SimpleDateFormat;
+import java.util.List;
 
 import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import javax.inject.Inject;
-import javax.inject.Named;
 
-import org.eclipse.core.databinding.DataBindingContext;
-import org.eclipse.core.databinding.beans.PojoProperties;
-import org.eclipse.core.databinding.observable.value.IObservableValue;
-import org.eclipse.e4.core.di.annotations.Optional;
-import org.eclipse.e4.ui.di.Persist;
-import org.eclipse.e4.ui.model.application.ui.MDirtyable;
-import org.eclipse.e4.ui.services.IServiceConstants;
-import org.eclipse.jface.databinding.swt.ISWTObservableValue;
-import org.eclipse.jface.databinding.swt.WidgetProperties;
+import org.eclipse.e4.ui.di.Focus;
+import org.eclipse.jface.viewers.ArrayContentProvider;
+import org.eclipse.jface.viewers.ComboViewer;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.ISelectionChangedListener;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.viewers.LabelProvider;
+import org.eclipse.jface.viewers.SelectionChangedEvent;
+import org.eclipse.jface.viewers.StructuredSelection;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.events.ModifyEvent;
-import org.eclipse.swt.events.ModifyListener;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.DateTime;
-import org.eclipse.swt.widgets.Label;
-import org.eclipse.swt.widgets.Text;
 
+import com.example.e4.rcp.todo.model.ITodoModel;
 import com.example.e4.rcp.todo.model.Todo;
 
 public class TodoDeletionPart {
+	@Inject
+	private ITodoModel model;
+	private ComboViewer viewer;
 
 	@PostConstruct
-	public void createControls(Composite parent) {
+	public void postConstruct(Composite parent) {
+		parent.setLayout(new GridLayout(2, false));
+		viewer = new ComboViewer(parent, SWT.DROP_DOWN);
+		viewer.setLabelProvider(new LabelProvider() {
+			@Override
+			public String getText(Object element) {
+				Todo todo = (Todo) element;
+				return todo.getSummary();
+			}
+		});
+		viewer.setContentProvider(ArrayContentProvider.getInstance());
+
+		List<Todo> todos = model.getTodos();
+		updateViewer(todos);
+
+		Button button = new Button(parent, SWT.PUSH);
+		button.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				ISelection selection = viewer.getSelection();
+				IStructuredSelection sel = (IStructuredSelection) selection;
+				if (sel.size() > 0) {
+					Todo firstElement = (Todo) sel.getFirstElement();
+					model.deleteTodo(firstElement.getId());
+					updateViewer(model.getTodos());
+				}
+
+			}
+		});
+		button.setText("Delete Selection");
 	}
-	
-	
+
+	private void updateViewer(List<Todo> todos) {
+		viewer.setInput(todos);
+		if (todos.size() > 0) {
+			viewer.setSelection(new StructuredSelection(todos.get(0)));
+		}
+	}
+
+	@Focus
+	public void focus() {
+		viewer.getCombo().setFocus();
+	}
 }
