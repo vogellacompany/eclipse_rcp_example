ada04bee50e0212755aa3ea8bae1b81d7dc70d7c
Lars Vogel <Lars.Vogel@gmail.com>
2012-10-16 09:31:50 +0200

    MapPart implemented and contributed to application model

--- com.example.e4.rcp.todo.contribute/src/com/vogella/e4/todo/contribute/parts/MapPart.java
+++ com.example.e4.rcp.todo.contribute/src/com/vogella/e4/todo/contribute/parts/MapPart.java
@@ -1,7 +1,38 @@
 package com.vogella.e4.todo.contribute.parts;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
+import javax.annotation.PostConstruct;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.browser.Browser;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.layout.GridData;
+
 public class MapPart {
-	public MapPart() {
-		System.out.println("Called");
+	private Text text;
+
+	@PostConstruct
+	public void postConstruct(Composite parent) {
+		parent.setLayout(new GridLayout(2, false));
+		try {
+			new Label(parent, SWT.NONE);
+			
+			text = new Text(parent, SWT.BORDER | SWT.H_SCROLL | SWT.SEARCH | SWT.CANCEL);
+			text.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false, false, 1, 1));
+			new Label(parent, SWT.NONE);
+			Browser browser = new Browser(parent, SWT.NONE);
+			browser.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
+			browser.setUrl("http://maps.google.de/maps?q="
+					+ URLEncoder.encode("Hamburg", "UTF-8")
+					+ "&output=embed");
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+
 	}
 }
