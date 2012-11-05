7a421a2f85f9527349a8f7dc9c9f6935599bc6ce
Lars Vogel <Lars.Vogel@gmail.com>
2012-11-05 12:34:06 +0100

    Uses correct package for calling the CSS themes

--- com.example.e4.rcp.todo/src/com/example/e4/rcp/todo/handlers/ThemeSwitchHandler.java
+++ com.example.e4.rcp.todo/src/com/example/e4/rcp/todo/handlers/ThemeSwitchHandler.java
@@ -4,20 +4,20 @@ import org.eclipse.e4.core.di.annotations.Execute;
 import org.eclipse.e4.ui.css.swt.theme.IThemeEngine;
 
 public class ThemeSwitchHandler {
-	static boolean doIt= true;
+	// Remember the state
+	static boolean defaulttheme= true;
 	@Execute
 	public void switchTheme(IThemeEngine engine) {
 		System.out.println("ThemeSwitchHandler called");
 		// The last argument controls
 		// whether the change should be persisted and
 		// restored on restart
-		if (doIt) {
-			engine.setTheme("de.vogella.e4.todo.darktheme", true);
+		if (!defaulttheme) {
+			engine.setTheme("com.vogella.e4.todo.defaulttheme", true);
 			
 		} else {
-			engine.setTheme("de.vogella.e4.todo.redtheme", true);
+			engine.setTheme("com.vogella.e4.todo.redtheme", true);
 		}
-		doIt= !doIt;
-		
+		defaulttheme= !defaulttheme;
 	}
 }
