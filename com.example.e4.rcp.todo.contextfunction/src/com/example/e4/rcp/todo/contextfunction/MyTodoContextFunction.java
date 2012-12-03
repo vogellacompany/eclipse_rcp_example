8e3703fa24f42328dd6be159fc983347264297fb
Lars Vogel <lars.vogel@gmail.com>
2012-12-03 16:08:43 +0100

    Adding an example project for a context function

---
+++ com.example.e4.rcp.todo.contextfunction/src/com/example/e4/rcp/todo/contextfunction/MyTodoContextFunction.java
@@ -0,0 +1,20 @@
+package com.example.e4.rcp.todo.contextfunction;
+
+import org.eclipse.e4.core.contexts.IContextFunction;
+import org.eclipse.e4.core.contexts.IEclipseContext;
+
+import com.example.e4.rcp.todo.model.Todo;
+
+public class MyTodoContextFunction implements IContextFunction {
+
+	@Override
+	public Object compute(IEclipseContext context) {
+		System.out.println("ContextFunction called");
+		Object object = context.get("id");
+		if (object!=null) {
+			
+		}
+		return new Todo();
+	}
+
+}
