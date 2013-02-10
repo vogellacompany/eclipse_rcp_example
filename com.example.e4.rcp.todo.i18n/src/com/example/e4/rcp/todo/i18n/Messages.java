ea30d36f3df693cb31758e630c163d9fe1ff15b9
Lars Vogel <lars.vogel@gmail.com>
2013-02-10 23:36:09 +0100

    Added translation plug-in to eclipse 4 project

---
+++ com.example.e4.rcp.todo.i18n/src/com/example/e4/rcp/todo/i18n/Messages.java
@@ -0,0 +1,16 @@
+package com.example.e4.rcp.todo.i18n;
+
+import org.eclipse.osgi.util.NLS;
+
+public class Messages extends NLS {
+	private static final String BUNDLE_NAME = "com.example.e4.rcp.todo.i18n.messages"; //$NON-NLS-1$
+	public static String TodoOverviewPart_0;
+	public static String TodoDeletionPart_0;
+	static {
+		// initialize resource bundle
+		NLS.initializeMessages(BUNDLE_NAME, Messages.class);
+	}
+
+	private Messages() {
+	}
+}
