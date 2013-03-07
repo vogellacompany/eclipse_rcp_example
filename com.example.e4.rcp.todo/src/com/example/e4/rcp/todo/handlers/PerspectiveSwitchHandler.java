f41bd661c56aa29579e408c4e48176621f35a426
Lars Vogel <lars.vogel@gmail.com>
2013-03-07 17:28:05 +0100

    Formatting of the methods parameter improved

--- com.example.e4.rcp.todo/src/com/example/e4/rcp/todo/handlers/PerspectiveSwitchHandler.java
+++ com.example.e4.rcp.todo/src/com/example/e4/rcp/todo/handlers/PerspectiveSwitchHandler.java
@@ -11,7 +11,8 @@ import org.eclipse.e4.ui.workbench.modeling.EPartService;
 public class PerspectiveSwitchHandler {
 	@Execute
 	public void switchPersepctive(MPerspective activePerspective,
-			MApplication app, EPartService partService,
+			MApplication app, 
+			EPartService partService,
 			EModelService modelService) {
 		List<MPerspective> perspectives = modelService.findElements(app, null,
 				MPerspective.class, null);
