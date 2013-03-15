bd53d1f81f6f382d4df3cd07c42c211f6b7105b6
Lars Vogel <lars.vogel@gmail.com>
2013-03-15 14:09:10 +0100

    Using a ContextFunction for ITodoModel

---
+++ com.example.e4.rcp.todo.service/src/com/example/e4/rcp/todo/service/internal/TodoServiceContextFunction.java
@@ -0,0 +1,22 @@
+package com.example.e4.rcp.todo.service.internal;
+
+import org.eclipse.e4.core.contexts.ContextInjectionFactory;
+import org.eclipse.e4.core.contexts.IEclipseContext;
+import org.eclipse.e4.ui.model.application.MApplication;
+
+import com.example.e4.rcp.todo.model.ITodoModel;
+
+public class TodoServiceContextFunction extends
+		org.eclipse.e4.core.contexts.ContextFunction {
+
+	@Override
+	public Object compute(IEclipseContext context) {
+		System.out.println("Creating a new service");
+		ITodoModel model = ContextInjectionFactory.make(MyTodoModelImpl.class,
+				context);
+		MApplication application = context.get(MApplication.class);
+		IEclipseContext ctx = application.getContext();
+		ctx.set(ITodoModel.class, model);
+		return model;
+	}
+}
