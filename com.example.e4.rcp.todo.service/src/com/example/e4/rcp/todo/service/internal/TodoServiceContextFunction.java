718ba9d31664f464dd6666d98ff4896f560d9c9f
Lars Vogel <lars.vogel@gmail.com>
2013-03-24 11:21:55 +0100

    Updated Context function

--- com.example.e4.rcp.todo.service/src/com/example/e4/rcp/todo/service/internal/TodoServiceContextFunction.java
+++ com.example.e4.rcp.todo.service/src/com/example/e4/rcp/todo/service/internal/TodoServiceContextFunction.java
@@ -1,14 +1,13 @@
 package com.example.e4.rcp.todo.service.internal;
 
+import org.eclipse.e4.core.contexts.ContextFunction;
 import org.eclipse.e4.core.contexts.ContextInjectionFactory;
 import org.eclipse.e4.core.contexts.IEclipseContext;
 import org.eclipse.e4.ui.model.application.MApplication;
 
 import com.example.e4.rcp.todo.model.ITodoModel;
 
-public class TodoServiceContextFunction extends
-		org.eclipse.e4.core.contexts.ContextFunction {
-
+public class TodoServiceContextFunction extends ContextFunction {
 	@Override
 	public Object compute(IEclipseContext context) {
 		System.out.println("Creating a new service");
