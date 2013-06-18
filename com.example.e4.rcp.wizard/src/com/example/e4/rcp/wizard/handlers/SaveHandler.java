61705e1842425b8aca98f009acb75daf99f4aa8e
Lars Vogel <Lars.Vogel@gmail.com>
2013-06-18 12:09:48 +0200

    Updated the wizard example

--- com.example.e4.rcp.wizard/src/com/example/e4/rcp/wizard/handlers/SaveHandler.java
+++ com.example.e4.rcp.wizard/src/com/example/e4/rcp/wizard/handlers/SaveHandler.java
@@ -15,10 +15,9 @@ import java.lang.reflect.InvocationTargetException;
 import javax.inject.Named;
 
 import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.e4.core.contexts.ContextInjectionFactory;
 import org.eclipse.e4.core.contexts.IEclipseContext;
-import org.eclipse.e4.core.di.annotations.Execute;
 import org.eclipse.e4.core.di.annotations.CanExecute;
+import org.eclipse.e4.core.di.annotations.Execute;
 import org.eclipse.e4.ui.model.application.MContribution;
 import org.eclipse.e4.ui.model.application.ui.MDirtyable;
 import org.eclipse.e4.ui.services.IServiceConstants;
