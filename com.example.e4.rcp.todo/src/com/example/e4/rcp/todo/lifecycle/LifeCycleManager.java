f41d4acecbe43a4571a62b619945d2edd4f1a850
Lars Vogel <Lars.Vogel@gmail.com>
2013-08-30 10:06:52 +0200

    Life cycle handler now centered

--- com.example.e4.rcp.todo/src/com/example/e4/rcp/todo/lifecycle/LifeCycleManager.java
+++ com.example.e4.rcp.todo/src/com/example/e4/rcp/todo/lifecycle/LifeCycleManager.java
@@ -8,6 +8,9 @@ import org.eclipse.e4.ui.workbench.lifecycle.PostContextCreate;
 import org.eclipse.equinox.app.IApplicationContext;
 import org.eclipse.jface.window.Window;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Rectangle;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Monitor;
 import org.eclipse.swt.widgets.Shell;
 import org.osgi.service.prefs.BackingStoreException;
 
@@ -22,18 +25,22 @@ public class LifeCycleManager {
 	private String user;
 
 	@PostContextCreate
-	public void postContextCreate(@Preference IEclipsePreferences prefs, IApplicationContext appContext) {
-	
+	public void postContextCreate(@Preference IEclipsePreferences prefs,
+			IApplicationContext appContext, Display display) {
+
 		final Shell shell = new Shell(SWT.TOOL | SWT.NO_TRIM);
 		PasswordDialog dialog = new PasswordDialog(shell);
 		if (user != null) {
 			dialog.setUser(user);
 		}
-		
-		// close the static splash screen 
+
+		// close the static splash screen
 		appContext.applicationRunning();
+
+		// position the shell
+		 setLocation(display, shell);
 		
-		// Open the dialog
+		// open the dialog
 		if (dialog.open() != Window.OK) {
 			// close the application
 			System.exit(-1);
@@ -46,5 +53,15 @@ public class LifeCycleManager {
 				e.printStackTrace();
 			}
 		}
+		
+	}
+
+	private void setLocation(Display display, Shell shell) {
+		Monitor monitor = display.getPrimaryMonitor();
+		Rectangle monitorRect = monitor.getBounds();
+		Rectangle shellRect = shell.getBounds();
+		int x = monitorRect.x + (monitorRect.width - shellRect.width) / 2;
+		int y = monitorRect.y + (monitorRect.height - shellRect.height) / 2;
+		shell.setLocation(x, y);
 	}
 }
