16699677e520299a18545343ded97add229827c6
Lars Vogel <Lars.Vogel@gmail.com>
2014-02-17 13:01:13 +0100

    EModelService has been released as API for Luna

--- com.example.e4.rcp.todo.contribute.processor/src/com/example/e4/rcp/todo/contribute/processor/AvailablePerspectiveProcessor.java
+++ com.example.e4.rcp.todo.contribute.processor/src/com/example/e4/rcp/todo/contribute/processor/AvailablePerspectiveProcessor.java
@@ -5,12 +5,12 @@ import java.util.List;
 import org.eclipse.e4.core.di.annotations.Execute;
 import org.eclipse.e4.ui.model.application.MApplication;
 import org.eclipse.e4.ui.model.application.ui.basic.MWindow;
-import org.eclipse.e4.ui.workbench.modeling.ModelService;
+import org.eclipse.e4.ui.workbench.modeling.EModelService;
 
 public class AvailablePerspectiveProcessor {
 
 	@Execute
-	public void execute(MApplication app, ModelService modelService) {
+	public void execute(MApplication app, EModelService modelService) {
 		List<MWindow> list = app.getChildren();
 		
 
