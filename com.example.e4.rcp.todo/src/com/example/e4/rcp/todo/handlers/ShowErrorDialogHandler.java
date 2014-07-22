dab873e8628093fcf9f507c4f1603f3c6f0b8647
Lars Vogel <Lars.Vogel@gmail.com>
2014-07-22 22:16:31 +0200

    Cleanup ErrorDialog

--- com.example.e4.rcp.todo/src/com/example/e4/rcp/todo/handlers/ShowErrorDialogHandler.java
+++ com.example.e4.rcp.todo/src/com/example/e4/rcp/todo/handlers/ShowErrorDialogHandler.java
@@ -8,9 +8,7 @@ import org.eclipse.core.runtime.MultiStatus;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.e4.core.di.annotations.Execute;
 import org.eclipse.e4.ui.model.application.ui.basic.MWindow;
-import org.eclipse.e4.ui.workbench.modeling.IWindowCloseHandler;
 import org.eclipse.jface.dialogs.ErrorDialog;
-import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.swt.widgets.Shell;
 
 public class ShowErrorDialogHandler {
@@ -18,31 +16,34 @@ public class ShowErrorDialogHandler {
 
 	@Execute
 	public void execute(final Shell shell, MWindow window) {
-
 		// create exception on purpose to demonstrate ErrorDialog
 		try {
 			String s = null;
 			System.out.println(s.length());
 		} catch (NullPointerException e) {
+			// I want to add the current stack trace
+			
 			MultiStatus status = createMultiStatus(e.getLocalizedMessage(), e);
 			ErrorDialog.openError(shell, "Error", "This is an error", status);
 		}
 	}
 
 	// helper method to show the stack trace in the detailed message
+	// not 
 	private static MultiStatus createMultiStatus(String msg, Throwable t) {
 
 		List<Status> childStatuses = new ArrayList<Status>();
+		StackTraceElement[] stackTraces = Thread.currentThread().getStackTrace();
 
-		for (String line : t.getStackTrace().toString().split(lineSeparator)) {
+ 		for (StackTraceElement stackTrace: stackTraces) {
 			Status status = new Status(IStatus.ERROR,
-					"com.example.e4.rcp.todo", line);
+					"com.example.e4.rcp.todo", stackTrace.toString());
 			childStatuses.add(status);
 		}
 
 		MultiStatus ms = new MultiStatus("com.example.e4.rcp.todo",
 				IStatus.ERROR, childStatuses.toArray(new Status[] {}),
-				t.getLocalizedMessage(), t);
+				t.toString(), t);
 		return ms;
 	}
 }
