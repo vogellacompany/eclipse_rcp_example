6f7ca7ed31f3989c1fa209114697fc1c647517d5
Simon Scholz <simon.scholz@vogella.com>
2014-09-03 16:42:19 +0200

    Added constants for the rest uris on the todo server
    Change-Id: I0d93b8ed5fe62e4add2115c7d5f2245ed2212e21
    Signed-off-by: Simon Scholz <simon.scholz@vogella.com>

---
+++ com.example.e4.rcp.todo.model/src/com/example/e4/rcp/todo/model/TodoRestUriConstants.java
@@ -0,0 +1,8 @@
+package com.example.e4.rcp.todo.model;
+
+public interface TodoRestUriConstants {
+	public static final String GET_todo = "/rest/todo/{id}";
+	public static final String GET_ALL_TODOS = "/rest/todos";
+	public static final String SAVE_todo = "/rest/todo/save";
+	public static final String DELETE_todo = "/rest/todo/delete/{id}";
+}
