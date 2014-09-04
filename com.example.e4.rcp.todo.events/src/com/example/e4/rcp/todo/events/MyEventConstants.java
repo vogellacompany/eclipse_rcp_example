306f62d49615ea4fe14820741240169ccfc20aa1
Lars Vogel <Lars.Vogel@gmail.com>
2014-09-04 12:13:17 +0200

    Update on the Eclipse 4.4 build setup and event processing

--- com.example.e4.rcp.todo.events/src/com/example/e4/rcp/todo/events/MyEventConstants.java
+++ com.example.e4.rcp.todo.events/src/com/example/e4/rcp/todo/events/MyEventConstants.java
@@ -2,13 +2,16 @@ package com.example.e4.rcp.todo.events;
 
 public interface MyEventConstants {
 
-	// Only used for event registration, you cannot
+	// this key can only used for event registration, you cannot
 	// send out generic events
-	String TOPIC_TODO_ALLTOPICS = "TOPIC_TODO/*";
+	String TOPIC_TODO_ALLTOPICS = "TOPIC_TODOS/*";
 
-	String TOPIC_TODO_NEW = "TOPIC_TODO/NEW";
+	// events to be send out
+	String TOPIC_TODOS_CHANGED = "TOPIC_TODOS/CHANGED";
+	
+	String TOPIC_TODO_NEW = "TOPIC_TODOS/TODO/NEW";
 
-	String TOPIC_TODO_DELETE = "TOPIC_TODO/DELETED";
+	String TOPIC_TODO_DELETE = "TOPIC_TODOS/TODO/DELETED";
 
-	String TOPIC_TODO_UPDATE = "TOPIC_TODO/UPDATED";
+	String TOPIC_TODO_UPDATE = "TOPIC_TODOS/TODO/UPDATED";
 }
