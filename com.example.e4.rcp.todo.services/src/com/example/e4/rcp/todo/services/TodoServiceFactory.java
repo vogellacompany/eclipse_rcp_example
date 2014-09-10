6faf5bdbf739b61d89f68798f7f671a77e9188cd
Lars Vogel <Lars.Vogel@vogella.com>
2014-09-10 10:15:38 +0200

    Adding the TodoServiceFactory as commented code

---
+++ com.example.e4.rcp.todo.services/src/com/example/e4/rcp/todo/services/TodoServiceFactory.java
@@ -0,0 +1,14 @@
+// Commented out as this has been replaced by an OSGi service or 
+// Context function, left here intentially for reference
+
+//package com.example.e4.rcp.todo.services;
+//
+//import com.example.e4.rcp.todo.model.ITodoService;
+//import com.example.e4.rcp.todo.services.internal.MyTodoServiceImpl;
+//
+//public class TodoServiceFactory {
+//	private static ITodoService todoService = new MyTodoServiceImpl();
+//	public static ITodoService getInstance() {
+//		return todoService;
+//	}
+//}
