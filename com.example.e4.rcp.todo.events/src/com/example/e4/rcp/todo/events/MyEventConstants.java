71fd35f4fa80c6232a1575f40b93d573c9847e66
Lars Vogel <Lars.Vogel@vogella.com>
2014-09-12 10:45:35 +0200

    Using constant class instead of interface.
    See #12

--- com.example.e4.rcp.todo.events/src/com/example/e4/rcp/todo/events/MyEventConstants.java
+++ com.example.e4.rcp.todo.events/src/com/example/e4/rcp/todo/events/MyEventConstants.java
@@ -1,17 +1,21 @@
 package com.example.e4.rcp.todo.events;
 
-public interface MyEventConstants {
-
+public final class MyEventConstants {
+	
+	private MyEventConstants() {
+		// prevent instantiation
+	}
+	
 	// this key can only used for event registration, you cannot
 	// send out generic events
-	String TOPIC_TODO_ALLTOPICS = "TOPIC_TODOS/*";
+	public static final String TOPIC_TODO_ALLTOPICS = "TOPIC_TODOS/*";
 
 	// events to be send out
-	String TOPIC_TODOS_CHANGED = "TOPIC_TODOS/CHANGED";
+	public static final String TOPIC_TODOS_CHANGED = "TOPIC_TODOS/CHANGED";
 	
-	String TOPIC_TODO_NEW = "TOPIC_TODOS/TODO/NEW";
+	public static final String TOPIC_TODO_NEW = "TOPIC_TODOS/TODO/NEW";
 
-	String TOPIC_TODO_DELETE = "TOPIC_TODOS/TODO/DELETED";
+	public static final String TOPIC_TODO_DELETE = "TOPIC_TODOS/TODO/DELETED";
 
-	String TOPIC_TODO_UPDATE = "TOPIC_TODOS/TODO/UPDATED";
+	public static final String TOPIC_TODO_UPDATE = "TOPIC_TODOS/TODO/UPDATED";
 }
