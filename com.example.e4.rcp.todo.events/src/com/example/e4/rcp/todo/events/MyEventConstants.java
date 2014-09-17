55d684ba9429700e0e2d75cb253b6b3bbf3f142a
Lars Vogel <Lars.Vogel@vogella.com>
2014-09-17 18:41:17 +0200

    Changes MyEventConstants back to an interfaces
    Fixes #12

--- com.example.e4.rcp.todo.events/src/com/example/e4/rcp/todo/events/MyEventConstants.java
+++ com.example.e4.rcp.todo.events/src/com/example/e4/rcp/todo/events/MyEventConstants.java
@@ -1,21 +1,17 @@
 package com.example.e4.rcp.todo.events;
 
-public final class MyEventConstants {
-	
-	private MyEventConstants() {
-		// prevent instantiation
-	}
-	
+public interface MyEventConstants {
+
 	// this key can only used for event registration, you cannot
 	// send out generic events
-	public static final String TOPIC_TODO_ALLTOPICS = "TOPIC_TODOS/*";
+	String TOPIC_TODO_ALLTOPICS = "TOPIC_TODOS/*";
 
 	// events to be send out
-	public static final String TOPIC_TODOS_CHANGED = "TOPIC_TODOS/CHANGED";
+	String TOPIC_TODOS_CHANGED = "TOPIC_TODOS/CHANGED";
 	
-	public static final String TOPIC_TODO_NEW = "TOPIC_TODOS/TODO/NEW";
+	String TOPIC_TODO_NEW = "TOPIC_TODOS/TODO/NEW";
 
-	public static final String TOPIC_TODO_DELETE = "TOPIC_TODOS/TODO/DELETED";
+	String TOPIC_TODO_DELETE = "TOPIC_TODOS/TODO/DELETED";
 
-	public static final String TOPIC_TODO_UPDATE = "TOPIC_TODOS/TODO/UPDATED";
+	String TOPIC_TODO_UPDATE = "TOPIC_TODOS/TODO/UPDATED";
 }
