96198a97589c9966f07d4f7844ee0d474e9befe6
Lars Vogel <Lars.Vogel@vogella.com>
2014-10-08 01:22:09 +0200

    Update from Berlin training

--- com.example.e4.rcp.todo.i18n/src/com/example/e4/rcp/todo/i18n/Messages.java
+++ com.example.e4.rcp.todo.i18n/src/com/example/e4/rcp/todo/i18n/Messages.java
@@ -1,17 +1,12 @@
 package com.example.e4.rcp.todo.i18n;
 
 public final class Messages {
-
-	public String part_overview;
-	public String part_details;
-
-	public String part_overview_button_loaddata;
-
+	public String buttonLoadData;
+	public String txtSummary;
+	public String txtDescription;
+	
 	public String part_deletion_button_deletetodo;
 
 	public String toolbar_main_changelocale;
-
-	public String todo_summary;
-	public String todo_description;
 
 }
