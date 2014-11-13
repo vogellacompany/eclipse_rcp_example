6e6cfa85fab133e6073a1c043f27d1a1b6d1412f
Lars Vogel <Lars.Vogel@vogella.com>
2014-11-13 15:54:37 +0100

    Using DI to create the wizard

--- com.example.e4.rcp.todo/src/com/example/e4/rcp/todo/handlers/NewTodoHandler.java
+++ com.example.e4.rcp.todo/src/com/example/e4/rcp/todo/handlers/NewTodoHandler.java
@@ -2,23 +2,41 @@ package com.example.e4.rcp.todo.handlers;
 
 import java.util.Date;
 
+import org.eclipse.e4.core.contexts.ContextInjectionFactory;
+import org.eclipse.e4.core.contexts.IEclipseContext;
 import org.eclipse.e4.core.di.annotations.Execute;
-import org.eclipse.e4.core.services.nls.Translation;
 import org.eclipse.jface.wizard.WizardDialog;
+import org.eclipse.jface.wizard.WizardPage;
 import org.eclipse.swt.widgets.Shell;
 
-import com.example.e4.rcp.todo.i18n.Messages;
 import com.example.e4.rcp.todo.model.ITodoService;
 import com.example.e4.rcp.todo.model.Todo;
 import com.example.e4.rcp.todo.wizards.TodoWizard;
+import com.example.e4.rcp.todo.wizards.TodoWizardPage1;
+import com.example.e4.rcp.todo.wizards.TodoWizardPage2;
 
 public class NewTodoHandler {
 	@Execute
-	public void execute(Shell shell, ITodoService model, @Translation Messages messages) {
+	public void execute(Shell shell, ITodoService model, IEclipseContext ctx) {
+		// create new context
+		IEclipseContext wizardCtx = ctx.createChild();
+		
+		// create todo and store in context
 		// use -1 to indicate a not existing id
 		Todo todo = new Todo(-1);
 		todo.setDueDate(new Date());
-		WizardDialog dialog = new WizardDialog(shell, new TodoWizard(todo, messages ));
+		wizardCtx.set(Todo.class, todo);
+
+		// create WizardPages via CIF
+		TodoWizardPage1 page1 = ContextInjectionFactory.make(TodoWizardPage1.class, wizardCtx);
+		wizardCtx.set(TodoWizardPage1.class, page1);
+		// no context needed for the creation
+		TodoWizardPage2 page2 = ContextInjectionFactory.make(TodoWizardPage2.class, null);
+		wizardCtx.set(TodoWizardPage2.class, page2);
+		
+		TodoWizard wizard = ContextInjectionFactory.make(TodoWizard.class, wizardCtx);
+		 
+		WizardDialog dialog = new WizardDialog(shell, wizard);
 		if (dialog.open()== WizardDialog.OK) {
 			model.saveTodo(todo);
 		}
--- com.example.e4.rcp.todo/src/com/example/e4/rcp/todo/wizards/TodoWizardPage1.java
+++ com.example.e4.rcp.todo/src/com/example/e4/rcp/todo/wizards/TodoWizardPage1.java
@@ -1,32 +1,33 @@
 package com.example.e4.rcp.todo.wizards;
 
+import javax.inject.Inject;
+
 import org.eclipse.jface.wizard.WizardPage;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
 
-import com.example.e4.rcp.todo.i18n.Messages;
 import com.example.e4.rcp.todo.model.Todo;
 import com.example.e4.rcp.todo.parts.TodoDetailsPart;
 
 public class TodoWizardPage1 extends WizardPage {
 
+	
 	private Todo todo;
-	private Messages messages;
-
-	public TodoWizardPage1(Todo todo, Messages messages) {
+	
+	@Inject
+	public TodoWizardPage1(Todo todo) {
 		super("page1");
 		this.todo = todo;
-		this.messages = messages;
 		setTitle("New Todo");
 		setDescription("Enter the todo data");
 	}
 
 	public void createControl(Composite parent) {
 		Composite container = new Composite(parent, SWT.NONE);
-		// We reuse the part and
-		// inject the values
+		// we could also create this class via DI but 
+		// in this example we stay with the next operator
 		TodoDetailsPart part = new TodoDetailsPart();
-		part.createControls(container, messages);
+		part.createControls(container);
 		part.setTodo(todo);
 		
 		setControl(container);
