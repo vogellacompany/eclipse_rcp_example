205f02f37e88147dac3e862562cbfb3efe9b49fa
Lars Vogel <Lars.Vogel@vogella.com>
2015-06-16 12:09:36 +0200

    Adjusts Wizard to consider setPageComplete

--- com.example.e4.rcp.todo/src/com/example/e4/rcp/todo/wizards/TodoWizard.java
+++ com.example.e4.rcp.todo/src/com/example/e4/rcp/todo/wizards/TodoWizard.java
@@ -2,16 +2,10 @@ package com.example.e4.rcp.todo.wizards;
 
 import javax.inject.Inject;
 
-import org.eclipse.jface.wizard.IWizardPage;
 import org.eclipse.jface.wizard.Wizard;
-
-import com.example.e4.rcp.todo.i18n.Messages;
-import com.example.e4.rcp.todo.model.Todo;
 
 public class TodoWizard extends Wizard {
 
-	boolean finish = false;
-	
 	@Inject
 	TodoWizardPage1 page1;
 	@Inject
@@ -25,7 +19,6 @@ public class TodoWizard extends Wizard {
 
 	@Override
 	public void addPages() {
-		
 		addPage(page1);
 		addPage(page2);
 	}
@@ -34,17 +27,5 @@ public class TodoWizard extends Wizard {
 	public boolean performFinish() {
 		return true;
 	}
-
-	@Override
-	public boolean canFinish() {
-		return finish;
-	}
-	
-	@Override
-	public IWizardPage getNextPage(IWizardPage page) {
-		return super.getNextPage(page);
-	}
-	
-	
 	
 }
