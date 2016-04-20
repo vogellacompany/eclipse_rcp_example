9ab129cc3d492bd8f4ab64ea3189f32e2d6be670
Simon Scholz <simon.scholz@vogella.com>
2016-04-20 09:24:14 +0200

    Updated restful TodoService to use @Component
    Change-Id: Ie1e244d22f810e1a70ccfe7b3486fd8fb99230c8
    Signed-off-by: Simon Scholz <simon.scholz@vogella.com>

--- com.example.e4.rcp.todo.rest.services/src/com/example/e4/rcp/todo/rest/services/internal/TodoServiceContextFunction.java
+++ com.example.e4.rcp.todo.rest.services/src/com/example/e4/rcp/todo/rest/services/internal/TodoServiceContextFunction.java
@@ -1,17 +1,18 @@
 package com.example.e4.rcp.todo.rest.services.internal;
 
-import org.eclipse.e4.core.contexts.ContextFunction;
 import org.eclipse.e4.core.contexts.ContextInjectionFactory;
+import org.eclipse.e4.core.contexts.IContextFunction;
 import org.eclipse.e4.core.contexts.IEclipseContext;
 import org.eclipse.e4.ui.model.application.MApplication;
+import org.osgi.service.component.annotations.Component;
 
 import com.example.e4.rcp.todo.model.ITodoService;
 
-public class TodoServiceContextFunction extends ContextFunction {
+@Component(name = "TodoService", service = IContextFunction.class, property = "service.context.key=com.example.e4.rcp.todo.model.ITodoService")
+public class TodoServiceContextFunction implements IContextFunction {
 	@Override
 	public Object compute(IEclipseContext context, String contextKey) {
-		ITodoService todoService = 
-				ContextInjectionFactory.make(MyRestfulTodoServiceImpl.class, context);
+		ITodoService todoService = ContextInjectionFactory.make(MyRestfulTodoServiceImpl.class, context);
 
 		MApplication app = context.get(MApplication.class);
 		IEclipseContext appCtx = app.getContext();
