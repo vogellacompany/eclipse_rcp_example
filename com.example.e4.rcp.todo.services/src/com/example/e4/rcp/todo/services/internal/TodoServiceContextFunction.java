f04f4af4e50bd9572fb1624637e37ee32ecb99c8
Lars Vogel <Lars.Vogel@vogella.com>
2016-06-16 14:36:53 +0200

    Updated solution to use DS annotations

--- com.example.e4.rcp.todo.services/src/com/example/e4/rcp/todo/services/internal/TodoServiceContextFunction.java
+++ com.example.e4.rcp.todo.services/src/com/example/e4/rcp/todo/services/internal/TodoServiceContextFunction.java
@@ -2,21 +2,34 @@ package com.example.e4.rcp.todo.services.internal;
 
 import org.eclipse.e4.core.contexts.ContextFunction;
 import org.eclipse.e4.core.contexts.ContextInjectionFactory;
+import org.eclipse.e4.core.contexts.IContextFunction;
 import org.eclipse.e4.core.contexts.IEclipseContext;
 import org.eclipse.e4.ui.model.application.MApplication;
+import org.osgi.service.component.annotations.Component;
 
 import com.example.e4.rcp.todo.model.ITodoService;
-
+@Component(service=IContextFunction.class,
+property="service.context.key=com.example.e4.rcp.todo.model.ITodoService")
 public class TodoServiceContextFunction extends ContextFunction {
+
 	@Override
 	public Object compute(IEclipseContext context, String contextKey) {
-		ITodoService todoService = 
+		MyTodoServiceImpl todoService = 
 				ContextInjectionFactory.make(MyTodoServiceImpl.class, context);
-
 		MApplication app = context.get(MApplication.class);
-		IEclipseContext appCtx = app.getContext();
-		appCtx.set(ITodoService.class, todoService);
-
+		app.getContext().set(ITodoService.class, todoService);
 		return todoService;
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
