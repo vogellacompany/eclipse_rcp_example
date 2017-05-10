b6938b80f2cf49810eb9e361a28c3a0078e27527
Lars Vogel <Lars.Vogel@vogella.com>
2017-05-10 08:09:07 +0200

    Cleanup of the code for 4.7

--- com.example.e4.rcp.todo.model/src/com/example/e4/rcp/todo/model/ITodoService.java
+++ com.example.e4.rcp.todo.model/src/com/example/e4/rcp/todo/model/ITodoService.java
@@ -6,10 +6,6 @@ import java.util.function.Consumer;
 
 public interface ITodoService {
 	
-	Tag<Tag<Todo>> getRootTag();
-	
-	List<Tag<Todo>> getTags(long id);
-	
 	void getTodos(Consumer<List<Todo>> todosConsumer);
 
 	boolean saveTodo(Todo todo);
--- com.example.e4.rcp.todo.services/src/com/example/e4/rcp/todo/services/internal/MyTodoServiceImpl.java
+++ com.example.e4.rcp.todo.services/src/com/example/e4/rcp/todo/services/internal/MyTodoServiceImpl.java
@@ -16,7 +16,6 @@ import org.eclipse.e4.core.services.events.IEventBroker;
 
 import com.example.e4.rcp.todo.events.MyEventConstants;
 import com.example.e4.rcp.todo.model.ITodoService;
-import com.example.e4.rcp.todo.model.Tag;
 import com.example.e4.rcp.todo.model.Todo;
 
 public class MyTodoServiceImpl implements ITodoService {
@@ -27,31 +26,27 @@ public class MyTodoServiceImpl implements ITodoService {
 	// use dependency injection in MyTodoServiceImpl
 	@Inject
 	private IEventBroker broker;
-	private Tag<Tag<Todo>> rootTag;
 
 	public MyTodoServiceImpl() {
 		todos = createInitialModel();
-		createRootTag(todos);
 	}
 
 	@Override
 	public void getTodos(Consumer<List<Todo>> todosConsumer) {
 		// always pass a new copy of the data
-		todosConsumer.accept(todos.stream().map(t -> t.copy()).collect(Collectors.toList()));
+		todosConsumer.accept(todos.stream().map(Todo::copy).collect(Collectors.toList()));
 	}
 
 	protected List<Todo> getTodosInternal() {
 		return todos;
 	}
 
-	// create or update an existing instance of Todo
+	// create or update an existing instance
 	@Override
 	public synchronized boolean saveTodo(Todo newTodo) {
-		// hold the Optional object as reference to determine, if the Todo is
-		// newly created or not
 		Optional<Todo> todoOptional = findById(newTodo.getId());
 
-		// get the actual todo or create a new one
+		// get the actual task or create a new one
 		Todo todo = todoOptional.orElse(new Todo(current.getAndIncrement()));
 		todo.setSummary(newTodo.getSummary());
 		todo.setDescription(newTodo.getDescription());
@@ -72,7 +67,7 @@ public class MyTodoServiceImpl implements ITodoService {
 
 	@Override
 	public Optional<Todo> getTodo(long id) {
-		return findById(id).map(todo -> todo.copy());
+		return findById(id).map(Todo::copy);
 	}
 
 	@Override
@@ -90,41 +85,19 @@ public class MyTodoServiceImpl implements ITodoService {
 		return deleteTodo.isPresent();
 	}
 
-	@Override
-	public Tag<Tag<Todo>> getRootTag() {
-		return rootTag;
-	}
-
-	@Override
-	public List<Tag<Todo>> getTags(long id) {
-		List<Tag<Todo>> tags = new ArrayList<>();
-	
-		Optional<Todo> findById = findById(id);
-		findById.ifPresent(todo -> findTags(todo, tags, getRootTag()));
-	
-		return tags;
-	}
-
-	// Example data, change if you like
+	// create example data
 	private List<Todo> createInitialModel() {
 		List<Todo> list = new ArrayList<>();
 		list.add(createTodo("Application model", "Flexible and extensible"));
 		list.add(createTodo("DI", "@Inject as programming mode"));
 		list.add(createTodo("OSGi", "Services"));
-		list.add(createTodo("SWT", "Widgets"));
+		list.add(createTodo("SWT", "Using widgets"));
 		list.add(createTodo("JFace", "Especially Viewers!"));
-		list.add(createTodo("CSS Styling", "Style your application"));
 		list.add(createTodo("Eclipse services", "Selection, model, Part"));
+		list.add(createTodo("CSS Styling", "Style your application"));
 		list.add(createTodo("Renderer", "Different UI toolkit"));
 		list.add(createTodo("Compatibility Layer", "Run Eclipse 3.x"));
 		return list;
-	}
-
-	private void createRootTag(List<Todo> todos) {
-		Tag<Todo> eclipseTag = new Tag<>("Eclipse", todos);
-		List<Tag<Todo>> tagList = new ArrayList<>();
-		tagList.add(eclipseTag);
-		rootTag = new Tag<>("root", tagList);
 	}
 
 	private Todo createTodo(String summary, String description) {
@@ -135,24 +108,11 @@ public class MyTodoServiceImpl implements ITodoService {
 		return getTodosInternal().stream().filter(t -> t.getId() == id).findAny();
 	}
 
-	private void findTags(Todo todo, List<Tag<Todo>> todosTags, Tag<?> rootTag) {
-		List<?> taggedElements = rootTag.getTaggedElements();
-		for (Object taggedElement : taggedElements) {
-			if (taggedElement instanceof Tag) {
-				findTags(todo, todosTags, (Tag<?>) taggedElement);
-			} else if (todo.equals(taggedElement)) {
-				@SuppressWarnings("unchecked")
-				Tag<Todo> foundTag = (Tag<Todo>) rootTag;
-				todosTags.add(foundTag);
-			}
-		}
-	}
-
 	private Map<String, String> createEventData(String topic, String todoId) {
 		Map<String, String> map = new HashMap<>();
 		// in case the receiver wants to check the topic
 		map.put(MyEventConstants.TOPIC_TODO, topic);
-		// which todo has changed
+		// which task has changed
 		map.put(Todo.FIELD_ID, todoId);
 		return map;
 	}
