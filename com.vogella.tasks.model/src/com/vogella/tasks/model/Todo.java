2196705f966aa4dd852773e43f93ec497e5d5e95
Lars Vogel <Lars.Vogel@vogella.com>
2020-07-23 18:07:59 +0200

    Adding databinding code to the example

--- com.vogella.tasks.model/src/com/vogella/tasks/model/Todo.java
+++ com.vogella.tasks.model/src/com/vogella/tasks/model/Todo.java
@@ -1,89 +1,108 @@
 package com.vogella.tasks.model;
 
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
 import java.util.Date;
 import java.util.Objects;
 
 public class Todo {
 
-    private final long id;
-    private String summary = "";
-    private String description = "";
-    private boolean done = false;
-    private Date dueDate = new Date();
+	private PropertyChangeSupport changes = new PropertyChangeSupport(this);
 
-    public Todo(long id, String summary, String description, boolean done, Date dueDate) {
-        this.id = id;
-        this.summary = summary;
-        this.description = description;
-        this.done = done;
-        this.dueDate = dueDate;
-    }
+	public static final String FIELD_ID = "id";
+	public static final String FIELD_SUMMARY = "summary";
+	public static final String FIELD_DESCRIPTION = "description";
+	public static final String FIELD_DONE = "done";
+	public static final String FIELD_DUEDATE = "dueDate";
 
-    public Todo(long id) {
-        this.id = id;
-    }
+	private final long id;
+	private String summary = "";
+	private String description = "";
+	private boolean done = false;
+	private Date dueDate = new Date();
 
-    public String getSummary() {
-        return summary;
-    }
+	public Todo(long id, String summary, String description, boolean done, Date dueDate) {
+		this.id = id;
+		this.summary = summary;
+		this.description = description;
+		this.done = done;
+		this.dueDate = dueDate;
+	}
 
-    public void setSummary(String summary) {
-        this.summary = summary;
-    }
+	public Todo(long id) {
+		this.id = id;
+	}
 
-    public String getDescription() {
-        return description;
-    }
+	public String getSummary() {
+		return summary;
+	}
 
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public boolean isDone() {
-        return done;
+	public void setSummary(String summary) {
+        changes.firePropertyChange(FIELD_SUMMARY, this.summary,
+                this.summary = summary);
     }
 
-    public void setDone(boolean done) {
-        this.done = done;
-    }
+	public String getDescription() {
+		return description;
+	}
+	   public void setDescription(String description) {
+	        changes.firePropertyChange(FIELD_DESCRIPTION, this.description, this.description = description);
+	    }
 
-    public Date getDueDate() {
-        return new Date(dueDate.getTime());
-    }
 
-    public void setDueDate(Date dueDate) {
-        this.dueDate = new Date(dueDate.getTime());
-    }
-    public long getId() {
-        return id;
-    }
+	public boolean isDone() {
+		return done;
+	}
 
-    @Override
-    public String toString() {
-        return "Todo [id=" + id + ", summary=" + summary + "]";
-    }
+	public void setDone(boolean isDone) {
+		changes.firePropertyChange(FIELD_DONE, this.done, this.done = isDone);
+	}
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
+	public Date getDueDate() {
+		return new Date(dueDate.getTime());
+	}
 
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        Todo other = (Todo) obj;
-        return id == other.id;
-    }
 
-    public Todo copy() {
-        return new Todo(this.id, this.summary,
-                this.description, this.done,
-                getDueDate());
-    }
+	public void setDueDate(Date dueDate) {
+		changes.firePropertyChange(FIELD_DUEDATE, this.dueDate, this.dueDate = new Date(dueDate.getTime()));
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	@Override
+	public String toString() {
+		return "Todo [id=" + id + ", summary=" + summary + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Todo other = (Todo) obj;
+		return id == other.id;
+	}
+
+	public Todo copy() {
+		return new Todo(this.id, this.summary, this.description, this.done, getDueDate());
+	}
+
+	public void addPropertyChangeListener(PropertyChangeListener l) {
+		changes.addPropertyChangeListener(l);
+	}
+
+	public void removePropertyChangeListener(PropertyChangeListener l) {
+		changes.removePropertyChangeListener(l);
+	}
 
 }
