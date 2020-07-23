126543c42bbefa2d067646eb481dc039ee6790a0
Lars Vogel <Lars.Vogel@vogella.com>
2020-07-23 17:59:30 +0200

    Adding events and update of Tycho build to 1.7.0

---
+++ com.vogella.tasks.events/src/com/vogella/tasks/events/MyEventConstants.java
@@ -0,0 +1,25 @@
+package com.vogella.tasks.events;
+/**
+ *
+ * @noimplement This interface is not intended to be implemented by clients.
+ *
+ * Only used for constant definition
+ */
+
+public interface MyEventConstants {
+
+    // topic identifier for all topics
+    String TOPIC_TODO = "TOPIC_TODOS";
+
+    // this key can only be used for event registration, you cannot
+    // send out generic events
+    String TOPIC_TODO_ALLTOPICS = "TOPIC_TODOS/*";
+
+    String TOPIC_TODOS_CHANGED = "TOPIC_TODOS/CHANGED";
+
+    String TOPIC_TODO_NEW = "TOPIC_TODOS/TODO/NEW";
+
+    String TOPIC_TODO_DELETE = "TOPIC_TODOS/TODO/DELETED";
+
+    String TOPIC_TODO_UPDATE = "TOPIC_TODOS/TODO/UPDATED";
+}
