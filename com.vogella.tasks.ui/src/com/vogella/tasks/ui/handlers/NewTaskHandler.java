3ac7824e9192b987b9745ccf54f78ee736467c33
Lars Vogel <Lars.Vogel@vogella.com>
2022-04-27 13:41:46 +0200

    Updates in the views

--- com.vogella.tasks.ui/src/com/vogella/tasks/ui/handlers/NewTaskHandler.java
+++ com.vogella.tasks.ui/src/com/vogella/tasks/ui/handlers/NewTaskHandler.java
@@ -1,9 +1,6 @@
 package com.vogella.tasks.ui.handlers;
 
-import java.time.LocalDate;
-
 import org.eclipse.e4.core.di.annotations.Execute;
-import org.eclipse.e4.core.services.events.IEventBroker;
 import org.eclipse.jface.window.Window;
 import org.eclipse.jface.wizard.WizardDialog;
 import org.eclipse.swt.widgets.Shell;
@@ -13,11 +10,11 @@ import com.vogella.tasks.model.TaskService;
 import com.vogella.tasks.ui.wizards.TaskWizard;
 
 public class NewTaskHandler {
+
     @Execute
-	public void execute(Shell shell, TaskService taskService, IEventBroker broker) {
+	public void execute(Shell shell, TaskService taskService) {
         // use -1 to indicate a not existing id
-        Task task = new Task(-1);
-        task.setDueDate(LocalDate.now());
+		var task = new Task(-1);
 		var dialog = new WizardDialog(shell, new TaskWizard(task));
         if (dialog.open() == Window.OK) {
             // call service to save task object
