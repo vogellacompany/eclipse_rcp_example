8eaed77604b2c0c3490beea36ac86d2f915def0b
Lars Vogel <Lars.Vogel@vogella.com>
2023-10-13 11:41:59 +0200

    Update of the image loader example code

--- com.vogella.service.imageloader/src/com/vogella/imageloader/services/IBundleResourceLoader.java
+++ com.vogella.service.imageloader/src/com/vogella/imageloader/services/IBundleResourceLoader.java
@@ -1,13 +1,18 @@
 package com.vogella.imageloader.services;
 
 import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.swt.graphics.Image;
 
 public interface IBundleResourceLoader {
 
-    // service does NOT recycle the provided image
+    // service does NOT dispose the provided image
     // the consumer of the image is responsible for calling the dispose()
     // method on the created Image object once the Image is not required anymore
     // can be automated with the usage of LocalResourceManager
 
     public ImageDescriptor getImageDescriptor(Class<?> clazz, String path);
+    
+    // service does NOT dispose the provided image nor the created
+    // caller needs to dispose them
+    public Image resize(Image image, int width, int height);
 }
--- com.vogella.service.imageloader/src/com/vogella/imageloader/services/internal/BundleResourceLoaderImpl.java
+++ com.vogella.service.imageloader/src/com/vogella/imageloader/services/internal/BundleResourceLoaderImpl.java
@@ -5,6 +5,10 @@ import java.net.URL;
 import org.eclipse.core.runtime.FileLocator;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.GC;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.widgets.Display;
 import org.osgi.framework.Bundle;
 import org.osgi.framework.FrameworkUtil;
 import org.osgi.service.component.annotations.Component;
@@ -22,4 +26,15 @@ public class BundleResourceLoaderImpl implements IBundleResourceLoader {
         return ImageDescriptor.createFromURL(url);
     }
 
+    @Override
+	public Image resize(Image image, int width, int height) {
+		Image scaled = new Image(Display.getDefault(), width, height);
+		GC gc = new GC(scaled);
+		gc.setAntialias(SWT.ON);
+		gc.setInterpolation(SWT.HIGH);
+		gc.drawImage(image, 0, 0, image.getBounds().width, image.getBounds().height, 0, 0, width, height);
+		gc.dispose();
+//		image.dispose(); // caller need to do this
+		return scaled;
+	}
 }
