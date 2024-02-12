6e7ac7b581bfa10dcfa9dd553f37d75db8def5f1
Lars Vogel <Lars.Vogel@vogella.com>
2024-02-12 18:00:33 +0100

    Adding an example for the resize of sash container elements

---
+++ com.vogella.tasks.ui/src/com/vogella/tasks/ui/handlers/ResizeSashChildren.java
@@ -0,0 +1,26 @@
+ 
+package com.vogella.tasks.ui.handlers;
+
+import org.eclipse.e4.core.di.annotations.Execute;
+import org.eclipse.e4.ui.model.application.ui.MUIElement;
+import org.eclipse.e4.ui.model.application.ui.basic.MPartSashContainer;
+import org.eclipse.e4.ui.model.application.ui.basic.MWindow;
+import org.eclipse.e4.ui.workbench.modeling.EModelService;
+
+
+public class ResizeSashChildren {
+	
+	@Execute
+	public void execute(EModelService modelService, MWindow window) {
+		// this code assume you main sash container uses the com.vogella.tasks.ui.partsashcontainer.main id
+		MUIElement muiElement = modelService.find("com.vogella.tasks.ui.partsashcontainer.main", window);
+		if (muiElement instanceof MPartSashContainer container) {
+			// we only handle the case in which we have two direct children
+			if (container.getChildren().size()==2) {
+				container.getChildren().get(0).setContainerData("300");
+				container.getChildren().get(1).setContainerData("700");
+			}
+		}
+	}
+		
+}
