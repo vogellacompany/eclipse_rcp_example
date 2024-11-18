9a8acfb1764c6343d83d792fbbc8bf7bc8a9d524
Lars Vogel <Lars.Vogel@vogella.com>
2024-11-18 16:02:17 +0100

    Update to Java 21

--- com.example.e4.rcp/src/com/example/e4/rcp/handlers/AboutHandler.java
+++ com.example.e4.rcp/src/com/example/e4/rcp/handlers/AboutHandler.java
@@ -1,5 +1,8 @@
 package com.example.e4.rcp.handlers;
 
+import java.lang.reflect.Field;
+import java.util.Properties;
+
 import org.eclipse.e4.core.di.annotations.Execute;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.swt.widgets.Shell;
@@ -7,6 +10,31 @@ import org.eclipse.swt.widgets.Shell;
 public class AboutHandler {
 	@Execute
 	public void execute(Shell shell) {
-		MessageDialog.openInformation(shell, "About", "Eclipse RCP Application");
+		try {
+			// Create an instance of Properties
+			Properties properties = new Properties();
+			properties.setProperty("key1", "value1");
+
+			// Access the private field 'defaults'
+			Field defaultsField = Properties.class.getDeclaredField("defaults");
+
+			// Set the field accessible
+			defaultsField.setAccessible(true);
+
+			// Get the value of the private field
+			Properties defaults = (Properties) defaultsField.get(properties);
+
+			// Output the value of the defaults field
+			System.out.println("Defaults: " + defaults);
+
+			// Cleanup: Reset the accessibility (optional)
+			defaultsField.setAccessible(false);
+			if (MessageDialog.openConfirm(shell, "Confirmation", "Defaults: " + defaults)) {
+			}
+
+		} catch (Exception e) {
+
+			MessageDialog.openInformation(shell, "Confirmation", "" + e.getLocalizedMessage());
+		}
 	}
 }
