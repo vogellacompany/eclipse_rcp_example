7e9912d82a8cf01674525052bc741b041cbac714
Lars Vogel <Lars.Vogel@vogella.com>
2025-10-15 14:40:11 +0200

    Adding an error message for the resizing handler

--- com.vogella.tasks.ui/src/com/vogella/tasks/ui/handlers/ResizeSashChildren.java
+++ com.vogella.tasks.ui/src/com/vogella/tasks/ui/handlers/ResizeSashChildren.java
@@ -1,6 +1,6 @@
- 
-package com.vogella.tasks.ui.handlers;
+ package com.vogella.tasks.ui.handlers;
 
+import org.eclipse.core.runtime.ILog;
 import org.eclipse.e4.core.di.annotations.Execute;
 import org.eclipse.e4.ui.model.application.ui.MUIElement;
 import org.eclipse.e4.ui.model.application.ui.basic.MPartSashContainer;
@@ -14,6 +14,10 @@ public class ResizeSashChildren {
 	public void execute(EModelService modelService, MWindow window) {
 		// this code assume you main sash container uses the com.vogella.tasks.ui.partsashcontainer.main id
 		MUIElement muiElement = modelService.find("com.vogella.tasks.ui.partsashcontainer.main", window);
+		if (muiElement == null) {
+			ILog.get().error("Could not find the main part sash container, check that you use the correct ID in your model");
+			return;
+		}
 		if (muiElement instanceof MPartSashContainer container) {
 			// we only handle the case in which we have two direct children
 			if (container.getChildren().size()==2) {
